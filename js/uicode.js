8b7d8433e2a8abd675a8585ca41a243f539224a8
Mario Bielert <mario.bielert@googlemail.com>
2020-10-09 11:09:44 +0200

    Hide color and pencil on "Neu" button

--- js/uicode.js
+++ js/uicode.js
@@ -96,11 +96,11 @@ function initTest()
 Vue.component("metric-legend", {
   "props": ["metric"],
   "template": "<li class=\"btn btn-light legend_item\" style=\"background-color: #FFFFFF; margin-top: 10px;\" v-on:click=\"metricPopup(metric.name)\">"
-            + "<div v-bind:class=\"metric.popupKey\" v-bind:style=\"{ backgroundColor: metric.color}\">"
+            + "<div v-if=\"metric.name\" v-bind:class=\"metric.popupKey\" v-bind:style=\"{ backgroundColor: metric.color}\">"
             + "&nbsp;" //"<img src=\"img/icons/droplet.svg\" width=\"24\" height=\"24\">"
             + "</div> &nbsp;"
             + "<span v-html=\"metric.htmlName\"></span>&nbsp;"
-            + "<img src=\"img/icons/pencil.svg\" width=\"28\" height=\"28\" />"
+            + "<img v-if=\"metric.name\" src=\"img/icons/pencil.svg\" width=\"28\" height=\"28\" />"
             + "</li>",
   "methods": {
     "metricPopup": function(metricName) {
