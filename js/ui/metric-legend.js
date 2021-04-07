173232071fa674b51fdfa6296ed5c4b90ac6b54d
AndyBuchwald <andy.b.new@gmail.com>
2021-04-07 14:29:03 +0200

    refactor: Moved editpen from metric-legend.js to style.css

--- js/ui/metric-legend.js
+++ js/ui/metric-legend.js
@@ -18,8 +18,8 @@ export const MetricLegend = {
     <li class="btn btn-light legend_item" style="background-color: #FFFFFF; margin-top: 10px;" v-on:click="metricPopup">
       <div v-if="metric.name" v-bind:class="metric.popupKey" v-bind:style="{ backgroundColor: metric.color }">
         &nbsp; ${/* "<img src=\"img/icons/droplet.svg\" width=\"24\" height=\"24\">" */''}
-      </div> &nbsp;
-      <span v-html="metric.htmlName"></span>&nbsp;
-      <img v-if="metric.name" src="img/icons/pencil.svg" width="28" height="28" />
+      </div>
+      <span v-html="metric.htmlName"></span>
+      <img v-if="metric.name" src="img/icons/pencil.svg" />
     </li>`
 }
