481ae66658655dae5a4304e1b7b46675c675948a
AndyBuchwald <71061979+AndyBuchwald@users.noreply.github.com>
2021-04-09 13:20:56 +0200

    feat: Single click metric deletion, fixes #41 (#52)
    feat: Single click metric deletion and only click on pencil triggers edit, fixes #41
    
    Co-authored-by: Thomas Ilsche <tilsche@users.noreply.github.com>

--- js/ui/metric-legend.js
+++ js/ui/metric-legend.js
@@ -12,14 +12,19 @@ export const MetricLegend = {
     metricPopup: function () {
       const metricBase = Store.getMetricBase(this.$props.metric.name)
       Store.setMetricPopup(metricBase, !this.$props.metric.popup)
+    },
+    trashcanClicked: function () {
+      const metricBase = Store.getMetricBase(this.$props.metric.name)
+      window.MetricQWebView.instances[0].deleteMetric(metricBase)
     }
   },
   template: `
-    <li class="btn btn-light legend_item" style="background-color: #FFFFFF; margin-top: 10px;" v-on:click="metricPopup">
+    <li class="btn btn-light legend_item" style="background-color: #FFFFFF; margin-top: 10px;" >
       <div v-if="metric.name" v-bind:class="metric.popupKey" v-bind:style="{ backgroundColor: metric.color }">
         &nbsp; ${/* "<img src=\"img/icons/droplet.svg\" width=\"24\" height=\"24\">" */''}
       </div>
       <span v-html="metric.htmlName"></span>
-      <img v-if="metric.name" src="img/icons/pencil.svg" />
+      <img v-if="metric.name" src="img/icons/pencil.svg" v-on:click="metricPopup" />
+      <img v-if="metric.name" src="img/icons/trash.svg" v-on:click="trashcanClicked" />
     </li>`
 }
