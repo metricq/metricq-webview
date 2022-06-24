1bc8e6dfb65def755c9dc47ce19ab973808cda83
Mario Bielert <mario.bielert@tu-dresden.de>
2022-06-24 10:18:03 +0200

    ui: change default export file format to pdf

--- src/configuration.js
+++ src/configuration.js
@@ -5,7 +5,7 @@ export class Configuration {
     this.popup = false
     this.key = 'configuration'
     this.lastWheelEvent = undefined
-    this.exportFormat = 'png'
+    this.exportFormat = 'pdf'
     this.exportWidth = 640
     this.exportHeight = 480
     this.legendDisplay = 'right'
