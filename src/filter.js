252fcbeb0525d3e29b4053773316923444b33769
AndyBuchwald <andy.buchwald@tu-dresden.de>
2022-06-29 11:05:13 +0200

    fixup: fixed filter in case metric value is 0

--- src/filter.js
+++ src/filter.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 export function importFilter () {
   Vue.filter('withDecimalPlaces', (value, digits) => {
-    if (!value) return ''
+    if (!value && value !== 0) return ''
     return value.toLocaleString('de', {
       minimumFractionDigits: digits,
       maximumFractionDigits: digits
